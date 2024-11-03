5ea903ab17c67f081af18c3be0e8bcb1a40eea8a
Kristopher <890207+0xinterface@users.noreply.github.com>
2024-11-03 11:11:05 +0800

    uses base64 response body

--- repositories.tf
+++ repositories.tf
@@ -95,6 +95,6 @@ data "external" "apt_key" {
     "echo $(jq -r '.signing_key') | base64 --decode | gpg --with-fingerprint --with-colons 2>/dev/null | awk -F: '/^fpr/ { print $10 }' | head -1 | jq --raw-input '{\"keyid\": .}'"
   ]
   query = {
-    signing_key = data.http.signing_keys[each.key].response_body
+    signing_key = data.http.signing_keys[each.key].response_body_base64
   }
 }
