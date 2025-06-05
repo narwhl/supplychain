0b0b6a72bb32a71910aa4b41f07e9381f4ddff0e
Kristopher <root@sandb0x.io>
2025-06-05 08:09:19 +0800

    deprecate apt source one-liner in favor of deb822 style

--- repositories.tf
+++ repositories.tf
@@ -3,7 +3,9 @@ locals {
     "docker" = {
       "apt" = {
         "signing_key_url" = "https://download.docker.com/linux/debian/gpg"
-        "source"          = "https://download.docker.com/linux/debian $RELEASE stable"
+        "uris"            = "https://download.docker.com/linux/debian"
+        "suites"          = "$RELEASE"
+        "components"      = "stable"
       }
       "dnf" = {
         "source" = "https://download.docker.com/linux/centos/docker-ce.repo"
@@ -12,7 +14,9 @@ locals {
     "gitlab" = {
       "apt" = {
         "signing_key_url" = "https://packages.gitlab.com/runner/gitlab-runner/gpgkey"
-        "source"          = "https://packages.gitlab.com/runner/gitlab-runner/debian $RELEASE main"
+        "uris"            = "https://packages.gitlab.com/runner/gitlab-runner/debian"
+        "suites"          = "$RELEASE"
+        "components"      = "main"
       }
       "dnf" = {
         "source" = "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/config_file.repo?os=alma&dist=${var.rhel_major_release.version}&source=script"
@@ -21,7 +25,9 @@ locals {
     "google-cloud-sdk" = {
       "apt" = {
         "signing_key_url" = "https://packages.cloud.google.com/apt/doc/apt-key.gpg"
-        "source"          = "https://packages.cloud.google.com/apt cloud-sdk main"
+        "uris"            = "https://packages.cloud.google.com/apt"
+        "suites"          = "cloud-sdk"
+        "components"      = "main"
       }
       "dnf" = {
         "source" = "https://artifact.narwhl.dev/upstream/google-cloud-sdk.repo"
@@ -30,7 +36,9 @@ locals {
     "grafana" = {
       "apt" = {
         "signing_key_url" = "https://apt.grafana.com/gpg.key"
-        "source"          = "https://apt.grafana.com stable main"
+        "uris"            = "https://apt.grafana.com"
+        "suites"          = "stable"
+        "components"      = "main"
       }
       "dnf" = {
         "source" = "https://artifact.narwhl.dev/upstream/grafana.repo"
@@ -39,7 +47,9 @@ locals {
     "hashicorp" = {
       "apt" = {
         "signing_key_url" = "https://apt.releases.hashicorp.com/gpg"
-        "source"          = "https://apt.releases.hashicorp.com $RELEASE main"
+        "uris"            = "https://apt.releases.hashicorp.com"
+        "suites"          = "$RELEASE"
+        "components"      = "main"
       }
       "dnf" = {
         "source" = "https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo"
@@ -48,7 +58,9 @@ locals {
     "microsoft" = {
       "apt" = {
         "signing_key_url" = "https://packages.microsoft.com/keys/microsoft.asc"
-        "source"          = "https://packages.microsoft.com/repos/azure-cli/ $RELEASE main"
+        "uris"            = "https://packages.microsoft.com/repos/azure-cli/"
+        "suites"          = "$RELEASE"
+        "components"      = "main"
       }
       "dnf" = {
         "source" = "https://packages.microsoft.com/config/rhel/${var.rhel_major_release.version}.0/prod.repo"
@@ -57,7 +69,8 @@ locals {
     "nvidia-container-toolkit" = {
       "apt" = {
         "signing_key_url" = "https://nvidia.github.io/libnvidia-container/gpgkey"
-        "source"          = "https://nvidia.github.io/libnvidia-container/stable/deb/amd64 /"
+        "uris"            = "https://nvidia.github.io/libnvidia-container/stable/deb/amd64"
+        "suites"          = "/"
       }
       "dnf" = {
         "source" = "https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo"
@@ -66,7 +79,9 @@ locals {
     "tailscale" = {
       "apt" = {
         "signing_key_url" = "https://pkgs.tailscale.com/stable/debian/${var.debian_major_release.codename}.gpg"
-        "source"          = "https://pkgs.tailscale.com/stable/debian $RELEASE main"
+        "uris"            = "https://pkgs.tailscale.com/stable/debian"
+        "suites"          = "$RELEASE"
+        "components"      = "main"
       }
       "dnf" = {
         "source" = "https://pkgs.tailscale.com/stable/centos/${var.rhel_major_release.version}/tailscale.repo"
